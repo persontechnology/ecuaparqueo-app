b38305ffbdceeed03625b31ad1bed2e5c2c8775d
persontechnology <persontechnologys@gmail.com>
2022-03-17 19:28:56 -0500

    end orden

--- app/Http/Livewire/OrdenMovilizacion/Vehiculo.php
+++ app/Http/Livewire/OrdenMovilizacion/Vehiculo.php
@@ -4,8 +4,8 @@ namespace App\Http\Livewire\OrdenMovilizacion;
 
 use App\Models\Espacio;
 use App\Models\Parqueadero;
+use App\Models\Vehiculo as ModelsVehiculo;
 use Livewire\Component;
-use PhpOffice\PhpSpreadsheet\Reader\Xls\Color\BIFF5;
 
 class Vehiculo extends Component
 {
@@ -13,27 +13,38 @@ class Vehiculo extends Component
     public $espacios;
     public $idPar;
 
-    public $search;
+    public $foo;
+    public $search = '';
+    public $page = 1;
+ 
+    protected $queryString = [
+        'foo',
+        'search' => ['except' => ''],
+        'page' => ['except' => 1],
+    ];
 
-    
-    public function mount()
-    {
-        $this->parqueaderos=Parqueadero::get();
-        $this->idPar=$this->parqueaderos->first()->id??null;
-        if($this->idPar){
-            $this->espacios = $this->parqueaderos->first()->espacios()->with(['vehiculo.tipoVehiculo', 'vehiculo.kilometraje'])->get();
-        }
-        
-        
-    }
     public function render()
     {
-        return view('livewire.orden-movilizacion.vehiculo');
+        $this->parqueaderos=Parqueadero::get();
+        
+        if($this->idPar){
+            $ex=Parqueadero::find($this->idPar)->espacios;
+        }else{
+            $ex=$this->parqueaderos->first()->espacios;
+        }
+        
+        $this->espacios=ModelsVehiculo::whereIn('id',$ex->pluck('vehiculo_id'))
+        ->where('placa','like','%'.$this->search.'%')->get();
+        
+        $data = array(
+            'parqueaderos'=>$this->parqueaderos,
+            'espacios'=>$this->espacios,
+        );
+        return view('livewire.orden-movilizacion.vehiculo',$data);
     }
 
     public function changeEvent($value)
     {
-       $this->idPar = $value?$value:$this->parqueaderos->first()->id;
-        $value?$this->espacios = Parqueadero::find($value)->espacios()->with(['vehiculo.tipoVehiculo', 'vehiculo.kilometraje'])->get():null;    
+        $this->idPar=$value;
     }
 }
--- resources/views/livewire/orden-movilizacion/vehiculo.blade.php
+++ resources/views/livewire/orden-movilizacion/vehiculo.blade.php
@@ -42,54 +42,49 @@
                 <!-- /sidebar search -->
 
 
-                @if ($espacios)
+                
 
                 <!-- Online users -->
                 <div class="sidebar-section">
-                    {{-- <div class="sidebar-section-header">
-                        <span class="font-weight-semibold">Vehículos disponibles</span>
-                        <div class="list-icons ml-auto">
-                            <a href="#sidebar-users" class="list-icons-item" data-toggle="collapse">
-                                <i class="icon-arrow-down12"></i>
-                            </a>
-                        </div>
-                    </div> --}}
-                    <form class="sidebar-section-body" action="#">
-                        <input wire:model="search" class="form-control" type="search" placeholder="Buscar por placa...">
-                    </form>
                     <div class="collapse show" id="sidebar-users">
                         <div class="sidebar-section-body">
-                            
-                                <ul class="media-list media-list-bordered" id="external-events-list">
+                            <input wire:model="search" class="form-control my-1" type="search" placeholder="Buscar por placa...">
+                            <ul class="media-list media-list-bordered" id="external-events-list">
+                            @if ($espacios->count()>0)
+                                
+                                    
                                     @foreach ($espacios as $esp)
-                                        <li class="media" style="cursor: move;" data-id="{{ $esp->id }}" data-placa="{{ $esp->vehiculo->placa }}-{{ $esp->vehiculo->numero_chasis }}">
-                                            @if (Storage::exists($esp->vehiculo->foto))
-                                                <a href="#" class="mr-3">
-                                                    <img src="{{ Storage::url($esp->vehiculo->foto) }}" width="36" height="36" class="rounded-circle" alt="">
-                                                </a>
+                                        <li class="media" style="cursor: move;" data-id="{{ $esp->id }}" data-placa="{{ $esp->placa }}-{{ $esp->numero_chasis }}">
+                                            @if (Storage::exists($esp->foto))
+                                                
+                                                    <img src="{{ Storage::url($esp->foto) }}" width="36" height="36" class="rounded-circle" alt="">
+                                                
                                             @endif
                                             <div class="media-body">
-                                                <a href="#" class="media-title font-weight-semibold">
-                                                    {{ $esp->vehiculo->placa }}
-                                                </a>
+                                                
+                                                    {{ $esp->placa }}
+                                                
                                                 <span class="font-size-xs text-muted d-block">
-                                                    {{ $esp->vehiculo->numero_chasis }}
+                                                    {{ $esp->numero_chasis }}
                                                 </span>
                                             </div>
                                             <div class="ml-3 align-self-center">
-                                                <span data-popup="tooltip" title="{{ $esp->vehiculo->estado }}" data-placement="right" class="badge badge-mark border-{{ $esp->vehiculo->color_estado }}"></span>
+                                                <span data-popup="tooltip" title="{{ $esp->estado }}" data-placement="right" class="badge badge-mark border-{{ $esp->color_estado }}"></span>
                                             </div>
                                         </li>
                                     @endforeach
                                     
-                                </ul>    
-                            
+                                
+                            @else
+                            <i class="alert-danger" role="alert">No existe vehículos con placa: {{ $search }}</i>
+                            @endif
+                        </ul>    
 
                         </div>
                     </div>
                 </div>
                 <!-- /online-users -->
-                @endif
+               
 
                 <!-- Filter -->
                 {{-- <div class="sidebar-section">
