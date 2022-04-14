7ed2086cfa735d8011d8ad1a12b452517d700602
Fabian1992 <fabi.lopez1992@gmail.com>
2022-04-13 20:07:55 -0500

    helpers

--- app/Http/Livewire/Reportes/Vehiculos/OrdenesVehiculos.php
+++ app/Http/Livewire/Reportes/Vehiculos/OrdenesVehiculos.php
@@ -2,14 +2,20 @@
 
 namespace App\Http\Livewire\Reportes\Vehiculos;
 
+use App\Models\OrdenMovilizacion;
 use App\Models\TipoVehiculo;
 use Livewire\Component;
 
 class OrdenesVehiculos extends Component
 {
+    public $tiposVehiculos = [];
+    public $ordenesMovilizacion = [];
+    public $rangoDeFechas = 'MES';
     public function render()
     {
-        $tipos = TipoVehiculo::get();
-        return view('livewire.reportes.vehiculos.ordenes-vehiculos',['tipos'=>$tipos]);
+        $this->tiposVehiculos = TipoVehiculo::get();
+        $this->ordenesMovilizacion = OrdenMovilizacion::get();
+        $this->rangoDeFechas = rangoFechas('MES');
+        return view('livewire.reportes.vehiculos.ordenes-vehiculos');
     }
 }
