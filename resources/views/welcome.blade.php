6bf2907f76af612033f48815976f0816b1708d21
persontechnology <persontechnologys@gmail.com>
2022-05-19 11:24:06 -0500

    foto welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,10 @@
 
 <div class="flex-fill">
     <div class="text-center mb-4">
-        <img src="{{ asset('global_assets/images/logo_icon_light.png') }}" class="img-fluid mb-4" height="230" alt="">
-        <h1 class="display-3 font-weight-semibold line-height-1 mb-2">Ecuaparqueo</h1>
+        <h3 class="display-3 font-weight-semibold line-height-1 mb-2">Ecuaparqueo</h3>
         <h5>Sistema de parqueo automatizado</h5>
+        <img src="{{ asset('img/traking.svg') }}" class="img-fluid mb-4" height="230" alt="">
+        
     </div>
 </div>
 @endsection
