270380fbd44a4f55a81de8bb099860202f7b5e74
persontechnology <persontechnologys@gmail.com>
2022-08-18 10:03:29 -0500

    placa ve1

--- app/Models/Vehiculo.php
+++ app/Models/Vehiculo.php
@@ -29,7 +29,12 @@ class Vehiculo extends Model
     // Deivid, numero movil y placa del vehiculo concatenados
     public function getNumeroMovilPlacaAttribute()
     {
-            return $this->numero_movil??''.' '.$this->placa??'';
+        if($this->numero_movil && $this->placa){
+            return $this->numero_movil.' '.$this->placa;
+        }else{
+            return '';
+        }
+            
     }
 
     // Deivid, un vehiculo esta en espacios de un parquadero
